d6b8060066dca3920f8ebb04df0f762d4638c2c6
Shravan Hariharan <shravanhariharan2@gmail.com>
2021-03-23 14:44:07 -0700

    Add solution code

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+  public static void main(String[] args) {
+    NumberGuesser numberGuesser = new NumberGuesser();
+    numberGuesser.playGame();
+  }
+}
--- NumberGuesser.java
+++ NumberGuesser.java
@@ -1,44 +1,87 @@
 import java.util.Scanner;
 
 public class NumberGuesser {
-  public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
-    System.out.print("Provide an upper bound number for guessing: ");
-    int upper = in.nextInt();
-    int num = 0;
-    int count = 0;
-    int min = Integer.MAX_VALUE;
-    boolean running = true;
-    while (running) {
-      int res = (int) (Math.random() * (upper - 1) + 1);
-      while(num != res) {
-        System.out.print("Guess a number between 1 and " + upper + ": ");
-        num = in.nextInt();
-        if (num < res) {
-          System.out.println("Incorrect. The answer is greater than this value");
-        } else {
-          System.out.println("Incorrect. The answer is less than this value");
-        }
-        count++;
-      }
-      in.nextLine();
-      min = Math.min(min, count);
-      System.out.println("Congrats! The number was: " + res + ".");
-      System.out.println("Number of guesses: " + count);
-      System.out.println("Fewest number of guesses for this range: " + min);
-      System.out.println("Play again? (Y)es, (N)o");
-      String ans = in.nextLine();
-      if(ans.equals("Y") || ans.equals("y")) {
-        num = 0;
-        count = 0;
-        continue;
-      } else if(ans.equals("N") || ans.equals("n")) {
-        running = false;
-      } else {
-        System.out.println("Invalid input");
+  private int currentGuess;
+  private int targetNum;
+  private int currentGuessesCount;
+  private int minGuessesCount;
+  private int minNumberGuessable;
+  private int maxNumberGuessable;
+  private Scanner in;
+
+  public NumberGuesser() {
+    this.in = new Scanner(System.in);
+    this.currentGuess = 0;
+    this.targetNum = Integer.MAX_VALUE;
+    this.currentGuessesCount = 0;
+    this.minGuessesCount = Integer.MAX_VALUE;
+    this.minNumberGuessable = 1;
+    this.maxNumberGuessable = 1;
+  }
+
+  public void playGame() {
+    this.maxNumberGuessable = this.queryIntegerInput("Provide an upper bound number for guessing: ");
+    boolean isGameRunning = true;
+    while (isGameRunning) {
+      this.runGuessSession();
+      this.displayCompletionStats();
+      isGameRunning = this.doesUserWantToPlayAgain();
+    }
+  }
+
+  private void runGuessSession() {
+    this.targetNum = this.getRandomNumberInGuessingRange();
+    while (this.currentGuess != this.targetNum) {
+      this.currentGuess = this.queryIntegerInput("Guess a number between 1 and " + this.maxNumberGuessable + ": ");
+      this.currentGuessesCount++;
+      if (this.currentGuess < this.targetNum) {
+        System.out.println("Incorrect. The answer is greater than this value");
+      } else if (this.currentGuess > this.targetNum) {
+        System.out.println("Incorrect. The answer is less than this value");
       }
     }
-    
-    in.close();
+    // Need to flush buffer since Scanner::nextInt does not read newlines
+    this.flushRemainingInputBuffer();
+  }
+
+  private int getRandomNumberInGuessingRange() {
+    return (int) (Math.random() * (this.maxNumberGuessable - this.minNumberGuessable) + this.minNumberGuessable);
+  }
+
+  private void flushRemainingInputBuffer() {
+    this.in.nextLine();
+  }
+
+  private void displayCompletionStats() {
+    this.minGuessesCount = Math.min(this.minGuessesCount, this.currentGuessesCount);
+    System.out.println("Congrats! The number was: " + this.targetNum + ".");
+    System.out.println("Number of guesses: " + this.currentGuessesCount);
+    System.out.println("Fewest number of guesses for this range: " + this.minGuessesCount);
+  }
+
+  private boolean doesUserWantToPlayAgain() {
+    String response = this.queryStringInput("Play again? (Y)es, (N)o: ");
+    if(response.equals("Y") || response.equals("y")) {
+      this.resetCurrentSessionStats();
+      return true;
+    }
+    return false;
+  }
+
+  private void resetCurrentSessionStats() {
+    this.currentGuessesCount = 0;
+    this.currentGuess = 0;
+  }
+
+  private String queryStringInput(String prompt) {
+    System.out.print(prompt);
+    String response = in.nextLine();
+    return response;
+  }
+
+  private int queryIntegerInput(String prompt) {
+    System.out.print(prompt);
+    int response = in.nextInt();
+    return response;
   }
 }
